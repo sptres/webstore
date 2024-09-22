20143ac3c1c21c32122eec68687e7a1659ac30e7
sptres <shawnpark2397@gmail.com>
2024-09-22 04:07:03 -0700

    admin dashboard mvp

---
+++ src/components/Nav.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { ReactNode, ComponentProps } from 'react';
+import { cn } from '@/lib/utils';
+
+export function Nav({ children }): { children: ReactNode } {
+  return (
+    <nav className="bg-primary text-primary-foreground flex justify-center px-4">
+      {children}
+    </nav>
+  );
+}
+
+export function NavLink(props: Omit<ComponentProps<typeof Link>, 'className'>) {
+  const pathname = usePathname();
+  return (
+    <Link
+      {...props}
+      className={cn(
+        'p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:hover:text-secondary-foreground',
+        pathname === props.href && 'bg-background text-foreground'
+      )}
+    />
+  );
+}
